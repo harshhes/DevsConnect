ce1064b731a5026043145b4055b6372eb2a96764
Harsh <harshgolu82@gmail.com>
2022-11-23 22:41:28 +0530

    update build.sh

--- devsconnect/build_files.sh
+++ devsconnect/build_files.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
 echo "BUILD START"
 python3.10 -m pip install -r requirements.txt
+python3.10 manage.py makemigrations --noinput 
+python3.10 manage.py migrate --noinput 
 python3.10 manage.py collectstatic --noinput --clear
 echo "BUILD END"
